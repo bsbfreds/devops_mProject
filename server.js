d8e2e6b05c9a6b86b136f7cba0a58e06606054d6
bsbfreds <44804935+bsbfreds@users.noreply.github.com>
2025-01-29 22:26:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/health', (req, res) => {
 
 app.use('*', (req, res) => {
 	res.send(
-		'<img width="50%" align="center" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTo39HTqrjJ4NY_NWYz3nF-6AfzCEjG8QAbbHEqEnECRAgpzsl4sIb8Fne8i7QZQQOWj3I&usqp=CAU">'
+		'<img width="50%" align="center" src="https://images.unsplash.com/photo-1512621776951-a57141f2eefd?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">'
 	);
 });
 
