e4044eec6f4b68162de8261b2c64131a915b8fad
bsbfreds <44804935+bsbfreds@users.noreply.github.com>
2025-03-06 18:05:48 +0530

    updating the image on the homepage

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/health', (req, res) => {
 
 app.use('*', (req, res) => {
 	res.send(
-		'<img width="50%" align="center" src="https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">'
+		'<img width="50%" align="center" src="https://images.unsplash.com/photo-1532012197267-da84d127e765?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">'
 	);
 });
 
